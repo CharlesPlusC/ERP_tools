51618b79933ee449ce68f630b6e5bbe8da49318f
Charles C <66725307+CharlesPlusC@users.noreply.github.com>
2023-12-11 10:22:49 +0000

    refactor main and start geiger-type functions

--- source/main.py
+++ source/main.py
@@ -3,66 +3,40 @@ import netCDF4 as nc
 import os
 from itertools import islice
 from tools.data_processing import calculate_satellite_fov, process_trajectory, sgp4_prop_TLE ,combine_lw_sw_data, extract_hourly_ceres_data
-from tools.plotting import plot_fov_radiance, create_animation
+from tools.plotting import plot_radiance_animation
 
-def main(dataset_path, TLE, jd_start, jd_end, dt, number_of_items=150):
+def main(dataset_path, TLE, jd_start, jd_end, dt, number_of_tsteps=150):
     # Load the CERES dataset
-    
     data = nc.Dataset(dataset_path)
 
     # TLE for OneWeb satellite
-    sgp4_ephem = sgp4_prop_TLE(TLE = TLE, jd_start = jd_start, jd_end = jd_end, dt = dt)
+    sgp4_ephem = sgp4_prop_TLE(TLE=TLE, jd_start=jd_start, jd_end=jd_end, dt=dt)
 
+    # Extract data from the CERES dataset
     ceres_times, lat, lon, lw_radiation_data, sw_radiation_data = extract_hourly_ceres_data(data)
 
+    # Combine longwave and shortwave radiation data
     combined_radiation_data = combine_lw_sw_data(lw_radiation_data, sw_radiation_data)
 
+    # Process the satellite trajectory
     lats, lons, alts, ceres_indices = process_trajectory(sgp4_ephem, ceres_times)
 
-    # Create animation frames
+    # Set the output folder for animations
     output_folder = "output/FOV_sliced_data"
-    os.makedirs(output_folder, exist_ok=True)
 
-    # Loop and call the function for combined data
-    for idx, (alt, sat_lat, sat_lon, ceres_idx) in islice(enumerate(zip(alts, lats, lons, ceres_indices)), number_of_items):
-        print("idx:", idx)
-        horizon_distance_km = calculate_satellite_fov(alt)
-
-        # # Call for LW radiation
-        # lw_plot_filename = os.path.join(output_folder, f'fov_lw_rad_{idx}.png')
-        # plot_fov_radiance("CERES_lw_flux", ceres_idx, lw_radiation_data, lat, lon, sat_lat, sat_lon, alt, horizon_distance_km, lw_plot_filename, ceres_times)
-
-        # # Call for SW radiation
-        # sw_plot_filename = os.path.join(output_folder, f'fov_sw_rad_{idx}.png')
-        # plot_fov_radiance("CERES_sw_flux", ceres_idx, sw_radiation_data, lat, lon, sat_lat, sat_lon, alt, horizon_distance_km, sw_plot_filename, ceres_times)
-
-        # Call for combined radiation
-        combined_plot_filename = os.path.join(output_folder, f'fov_lwsw_rad_{idx}.png')
-        plot_fov_radiance("CERES_combined_flux", ceres_idx, combined_radiation_data, lat, lon, sat_lat, sat_lon, alt, horizon_distance_km, combined_plot_filename, ceres_times)
-
-    # Create filenames lists
-    lw_filenames = [os.path.join(output_folder, f'fov_lw_rad_{idx}.png') for idx in range(number_of_items)]
-    sw_filenames = [os.path.join(output_folder, f'fov_sw_rad_{idx}.png') for idx in range(number_of_items)]
-    combined_filenames = [os.path.join(output_folder, f'fov_lwsw_rad_{idx}.png') for idx in range(number_of_items)]
-
-    # # Create LW animation
-    # lw_animation_path = os.path.join(output_folder, 'lw_flux_animation.gif')
-    # create_animation(lw_filenames, lw_animation_path)
-
-    # # Create SW animation
-    # sw_animation_path = os.path.join(output_folder, 'sw_flux_animation.gif')
-    # create_animation(sw_filenames, sw_animation_path)
-
-    # Create combined animation
-    combined_animation_path = os.path.join(output_folder, 'combined_flux_animation_nipy.gif')
-    create_animation(combined_filenames, combined_animation_path)
+    # Call the plot_radiance_animation function with the necessary parameters
+    plot_radiance_animation(alts=alts, lats=lats, lons=lons, ceres_indices=ceres_indices, 
+                            lw_radiation_data=lw_radiation_data, sw_radiation_data=sw_radiation_data, 
+                            combined_radiation_data=combined_radiation_data, lat=lat, lon=lon, ceres_times=ceres_times, 
+                            number_of_tsteps=number_of_tsteps, lw=True, sw=True, lwsw=True, output_folder=output_folder)
 
 if __name__ == "__main__":
-    #OneWeb TLE
-    TLE = "1 56719U 23068K   23330.91667824 -.00038246  00000-0 -10188+0 0  9993 \n2 56719  87.8995  84.9665 0001531  99.5722 296.6576 13.15663544 27411"
-    # oneweb_tle_time = TLE_time(oneweb_test_tle)
-    jd_start = 2460069.5000000 #force time to be within the CERES dataset
+    # Configuration for OneWeb TLE and CERES dataset
+    TLE = "1 56719U 23068K   23330.91667824 -.00038246  00000-0 -10188+0 0  9993\n2 56719  87.8995  84.9665 0001531  99.5722 296.6576 13.15663544 27411"
+    jd_start = 2460069.5000000  # Force time to be within the CERES dataset
     jd_end = jd_start + 1
-    dt = 60 #seconds
-    dataset_path = 'external/data/CERES_SYN1deg-1H_Terra-Aqua-MODIS_Ed4.1_Subset_20230501-20230630.nc' # Hourly data
-    main(dataset_path, TLE, jd_start, jd_end, dt, number_of_items=150)
+    dt = 60  # Seconds
+    dataset_path = 'external/data/CERES_SYN1deg-1H_Terra-Aqua-MODIS_Ed4.1_Subset_20230501-20230630.nc'  # Hourly data
+
+    # Execute the main function
+    main(dataset_path, TLE, jd_start, jd_end, dt, number_of_tsteps=12)
--- source/tools/plotting.py
+++ source/tools/plotting.py
@@ -1,14 +1,15 @@
 """ Plotting Module
 
 This module contains functions for plotting data, including FoV radiance in different projections.
-Add detailed module description here.
 
 """
-import matplotlib.pyplot as plt
 import numpy as np
-import cartopy.crs as ccrs
+import os
 import imageio
-from .data_processing import is_within_fov, is_within_fov_vectorized, sat_normal_surface_angle_vectorized
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+from itertools import islice
+from .data_processing import calculate_satellite_fov, is_within_fov, is_within_fov_vectorized, sat_normal_surface_angle_vectorized
 from .utilities import convert_ceres_time_to_date, lla_to_ecef
 
 def plot_fov_radiation_mesh(variable_name, time_index, radiation_data, lat, lon, sat_lat, sat_lon, horizon_dist, output_path, ceres_times):
@@ -47,112 +48,146 @@ def plot_fov_radiation_mesh(variable_name, time_index, radiation_data, lat, lon,
     plt.savefig(output_path)
     plt.close(fig)
 
-def plot_fov_radiance(variable_name, time_index, radiation_data, lat, lon, sat_lat, sat_lon, sat_alt, horizon_dist, output_path, ceres_times):
+def compute_radiance_at_sc(variable_name, time_index, radiation_data, lat, lon, sat_lat, sat_lon, sat_alt, horizon_dist, ceres_times):
     R = 6371  # Earth's radius in km
 
-    # Ensure the mesh grid creation is correct
+    # Mesh grid creation
     lon2d, lat2d = np.meshgrid(lon, lat)
 
-    # Call the lla_to_ecef function
+    # lla_to_ecef function calls and FOV calculations
     ecef_x, ecef_y, ecef_z = lla_to_ecef(lat2d, lon2d, np.zeros_like(lat2d))
     fov_mask = is_within_fov_vectorized(sat_lat, sat_lon, horizon_dist, lat2d, lon2d)
     radiation_data_fov = np.ma.masked_where(~fov_mask, radiation_data[time_index, :, :])
     cos_thetas = sat_normal_surface_angle_vectorized(sat_lat, sat_lon, lat2d[fov_mask], lon2d[fov_mask])
     cosine_factors_2d = np.zeros_like(radiation_data_fov)
     cosine_factors_2d[fov_mask] = cos_thetas
-    #I think the cosine factors might be wrong. Looks like they are stronger at one edge of the fov
-    
-    #plot cosine factors on map
-    fig = plt.figure(figsize=(10, 7))
-    ax = plt.axes(projection=ccrs.PlateCarree())
-    ax.coastlines()
-    vmin, vmax = (-1, 1)
-    cmap = 'nipy_spectral'
-    mesh_plot = ax.pcolormesh(lon2d, lat2d, cosine_factors_2d, cmap=cmap, vmin=vmin, vmax=vmax, transform=ccrs.PlateCarree())
-    cbar = plt.colorbar(mesh_plot, orientation='vertical', shrink=0.7)
-    cbar.set_label('Cosine Factors')
-    # Extract timestamp for title
-    timestamp = convert_ceres_time_to_date(ceres_times[time_index])
-    plt.title(f'{variable_name} - {timestamp}')
-    ax.gridlines(draw_labels=True)
-    #modiufy output path to include cosine factors
-    cosine_map_output_path = output_path[:-4] + "_cosine_map.png"
-    plt.savefig(cosine_map_output_path)
 
+    # Adjusting radiation data
     adjusted_radiation_data = radiation_data_fov * cosine_factors_2d
 
-    # Call the lla_to_ecef function
-    ecef_x, ecef_y, ecef_z = lla_to_ecef(lat2d, lon2d, np.zeros_like(lat2d))
-    # Stack the results
-    ecef_pixels = np.stack((ecef_x, ecef_y, ecef_z), axis=-1)
-
-    # Satellite ECEF Position 
+    # Satellite position and distance calculations
     sat_ecef = np.array(lla_to_ecef(sat_lat, sat_lon, sat_alt))
-
-    # Reshape for broadcasting
-    sat_ecef_reshaped = sat_ecef.reshape((1, 1, 3))
-
-    # Calculate vector difference
-    vector_diff = sat_ecef_reshaped - ecef_pixels
-
-    # Calculate the Euclidean distance
-    distances = np.linalg.norm(vector_diff, axis=2)
-    # Debugging: Print distances for a few pixels
-
-    #plot a map of the distances
-    fig = plt.figure(figsize=(10, 7))
-    ax = plt.axes(projection=ccrs.PlateCarree())
-    ax.coastlines()
-    cmap = 'nipy_spectral'
-    mesh_plot = ax.pcolormesh(lon2d, lat2d, distances, cmap=cmap,  transform=ccrs.PlateCarree())
-    cbar = plt.colorbar(mesh_plot, orientation='vertical', shrink=0.7)
-    cbar.set_label('Distance to Satellite (km)')
-    # Extract timestamp for title
-    timestamp = convert_ceres_time_to_date(ceres_times[time_index])
-    plt.title(f'{variable_name} - {timestamp}')
-    ax.gridlines(draw_labels=True)
-    #modiufy output path to include dist map
-    distance_map_output_path = output_path[:-4] + "_distance_map.png"
-    plt.savefig(distance_map_output_path)
+    ecef_pixels = np.stack((ecef_x, ecef_y, ecef_z), axis=-1)
+    vector_diff = sat_ecef.reshape((1, 1, 3)) - ecef_pixels
+    distances = np.linalg.norm(vector_diff, axis=2) * 1000  # Convert to meters
 
     # Radiation calculation
     delta_lat = np.abs(lat[1] - lat[0])
     delta_lon = np.abs(lon[1] - lon[0])
-    lat_radians = np.radians(lat2d)
-    area_pixel = R**2 * np.radians(delta_lat) * np.radians(delta_lon) * np.cos(lat_radians)
-    #convert area pixel to m^2
-    area_pixel = area_pixel * (1000**2)
-    #convert distances to m
-    distances = distances * 1000
-    P_rad = adjusted_radiation_data * area_pixel / (np.pi * distances**2) #this aligns with equation 11 in the paper- although does not include the ADM anisotropic factor
+    area_pixel = R**2 * np.radians(delta_lat) * np.radians(delta_lon) * np.cos(np.radians(lat2d)) * (1000**2)  # Convert to m^2
+    P_rad = adjusted_radiation_data * area_pixel / (np.pi * distances**2)
 
-    # print("radiation flux reaching satellite:", np.sum(P_rad))
-    # print("force due to radiation:", np.sum(P_rad) / 299792458)
-    # print("acceleration into a 1000kg satellite:", (np.sum(P_rad) / 299792458) / 1000)
+    # Returning the necessary data for plotting
+    return lon2d, lat2d, cosine_factors_2d, distances, P_rad, ceres_times, time_index, variable_name
 
-    # Plotting P_rad
+def plot_data_map(lon2d, lat2d, data, label, output_path, ceres_times, time_index, variable_name):
     fig = plt.figure(figsize=(10, 7))
     ax = plt.axes(projection=ccrs.PlateCarree())
     ax.coastlines()
-    vmin = 0
-    vmax = 1
     cmap = 'nipy_spectral'
-
-    mesh_plot = ax.pcolormesh(lon2d, lat2d, P_rad, cmap=cmap, vmin=vmin, vmax=vmax, transform=ccrs.PlateCarree())
-
+    mesh_plot = ax.pcolormesh(lon2d, lat2d, data, cmap=cmap, transform=ccrs.PlateCarree())
     cbar = plt.colorbar(mesh_plot, orientation='vertical', shrink=0.7)
-    cbar.set_label('Radiation Reaching Satellite from each pixel (W/m^2)')
-    #add the total radiation flux to the title
-
-    # Extract timestamp for title
+    cbar.set_label(label)
     timestamp = convert_ceres_time_to_date(ceres_times[time_index])
-    plt.title(f'rad source:{variable_name} \n {timestamp} \nrad flux:{np.sum(P_rad):.2f} W/m^2')
-
+    plt.title(f'{variable_name} - {timestamp}')
     ax.gridlines(draw_labels=True)
-    plt.savefig(output_path)
+    modified_output_path = output_path[:-4] + ".png"
+    plt.savefig(modified_output_path)
     plt.close(fig)
+
+def plot_radiance_maps(time_index, variable_name, radiation_data, lat, lon, sat_lat, sat_lon, sat_alt, horizon_dist, output_path, ceres_times, plot_cosine_factors=True, plot_distances=True, plot_radiation=True):
+    # Call compute_radiance_at_sc to get the necessary data
+    lon2d, lat2d, cosine_factors_2d, distances, P_rad, _, _, _ = compute_radiance_at_sc(variable_name, time_index, radiation_data, lat, lon, sat_lat, sat_lon, sat_alt, horizon_dist, ceres_times)
+    print("variable_name:", variable_name)
+    # Plotting the cosine factors
+    if plot_cosine_factors:
+        cosine_factors_output_path = output_path[:-4] + "_cosine_map.png"
+        plot_data_map(lon2d, lat2d, cosine_factors_2d, 'Cosine Factors', cosine_factors_output_path, ceres_times, time_index, variable_name)
+
+    # Plotting the distances
+    if plot_distances:
+        distances_output_path = output_path[:-4] + "_distance_map.png"
+        plot_data_map(lon2d, lat2d, distances, 'Distance to Satellite (km)', distances_output_path, ceres_times, time_index, variable_name)
+
+    # Plotting the radiation data
+    if plot_radiation:
+        total_radiation_flux = np.sum(P_rad)
+        radiation_output_path = output_path[:-4] + "_radiation_map.png"
+        plot_data_map(lon2d, lat2d, P_rad, f'Radiation Reaching Satellite from each pixel (W/m^2) \nrad flux:{total_radiation_flux:.2f} W/m^2', radiation_output_path, ceres_times, time_index, variable_name)
+
+def plot_radiance_animation(alts, lats, lons, ceres_indices, lw_radiation_data, sw_radiation_data, combined_radiation_data, lat, lon, ceres_times, number_of_tsteps, lw=True, sw=True, lwsw=True, output_folder="output/FOV_sliced_data"):
+    os.makedirs(output_folder, exist_ok=True)
+
+    lw_filenames, sw_filenames, combined_filenames = [], [], []
+
+    for idx, (alt, sat_lat, sat_lon, ceres_idx) in islice(enumerate(zip(alts, lats, lons, ceres_indices)), number_of_tsteps):
+        print("idx:", idx)
+        horizon_distance_km = calculate_satellite_fov(alt)
+
+        if lw:
+            lw_plot_filename = os.path.join(output_folder, f'fov_lw_rad_{idx}.png')
+            plot_radiance_maps(ceres_idx, "CERES_lw_flux", lw_radiation_data, lat, lon, sat_lat, sat_lon, alt, horizon_distance_km, lw_plot_filename, ceres_times)
+            lw_filenames.append(lw_plot_filename)
+
+        if sw:
+            sw_plot_filename = os.path.join(output_folder, f'fov_sw_rad_{idx}.png')
+            plot_radiance_maps(ceres_idx, "CERES_sw_flux", sw_radiation_data, lat, lon, sat_lat, sat_lon, alt, horizon_distance_km, sw_plot_filename, ceres_times)
+            sw_filenames.append(sw_plot_filename)
+
+        if lwsw:
+            combined_plot_filename = os.path.join(output_folder, f'fov_lwsw_rad_{idx}.png')
+            plot_radiance_maps(ceres_idx, "CERES_combined_flux", combined_radiation_data, lat, lon, sat_lat, sat_lon, alt, horizon_distance_km, combined_plot_filename, ceres_times)
+            combined_filenames.append(combined_plot_filename)
+
+    if lw:
+        lw_animation_path = os.path.join(output_folder, 'lw_flux_animation.gif')
+        create_animation(lw_filenames, lw_animation_path)
+
+    if sw:
+        sw_animation_path = os.path.join(output_folder, 'sw_flux_animation.gif')
+        create_animation(sw_filenames, sw_animation_path)
+
+    if lwsw:
+        combined_animation_path = os.path.join(output_folder, 'combined_flux_animation_nipy.gif')
+        create_animation(combined_filenames, combined_animation_path)
 
 # Function to create animation
 def create_animation(filenames, animation_path):
     images = [imageio.imread(filename) for filename in filenames]
     imageio.mimsave(animation_path, images, duration=0.5)
+
+def plot_radiance_geiger(alts, lats, lons, ceres_indices, lw_radiation_data, sw_radiation_data, combined_radiation_data, lat, lon, ceres_times, number_of_tsteps, lw=True, sw=True, lwsw=True, output_folder="output/FOV_sliced_data"):
+    os.makedirs(output_folder, exist_ok=True)
+
+    # Initialize cumulative radiation data arrays
+    cumulative_lw = np.zeros_like(lw_radiation_data[0, :, :]) if lw else None
+    cumulative_sw = np.zeros_like(sw_radiation_data[0, :, :]) if sw else None
+    cumulative_lwsw = np.zeros_like(combined_radiation_data[0, :, :]) if lwsw else None
+
+    for idx, (alt, sat_lat, sat_lon, ceres_idx) in enumerate(zip(alts, lats, lons, ceres_indices)):
+        if idx >= number_of_tsteps:
+            break
+
+        horizon_dist = calculate_satellite_fov(alt)
+
+        # Update cumulative radiation data
+        if lw:
+            _, _, _, _, lw_P_rad, _, _, _ = compute_radiance_at_sc("LW", ceres_idx, lw_radiation_data, lat, lon, sat_lat, sat_lon, alt, horizon_dist, ceres_times)
+            cumulative_lw += lw_P_rad
+        if sw:
+            _, _, _, _, sw_P_rad, _, _, _ = compute_radiance_at_sc("SW", ceres_idx, sw_radiation_data, lat, lon, sat_lat, sat_lon, alt, horizon_dist, ceres_times)
+            cumulative_sw += sw_P_rad
+        if lwsw:
+            _, _, _, _, lwsw_P_rad, _, _, _ = compute_radiance_at_sc("LWSW", ceres_idx, combined_radiation_data, lat, lon, sat_lat, sat_lon, alt, horizon_dist, ceres_times)
+            cumulative_lwsw += lwsw_P_rad
+
+        # Plot cumulative radiation data
+        if lw:
+            lw_output_path = os.path.join(output_folder, f"cumulative_lw_{idx}.png")
+            plot_local_fov(lon, lat, cumulative_lw, lw_output_path, "Cumulative LW Radiation")
+        if sw:
+            sw_output_path = os.path.join(output_folder, f"cumulative_sw_{idx}.png")
+            plot_local_fov(lon, lat, cumulative_sw, sw_output_path, "Cumulative SW Radiation")
+        if lwsw:
+            lwsw_output_path = os.path.join(output_folder, f"cumulative_lwsw_{idx}.png")
+            plot_local_fov(lon, lat, cumulative_lwsw, lwsw_output_path, "Cumulative LWSW Radiation")
