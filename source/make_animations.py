6f99d204b7797393674784a6ce61dd856a287106
edsml-ccc21 <66725307+CharlesPlusC@users.noreply.github.com>
2023-12-15 09:13:16 +0000

    why write better code when you can parallelize bad code ?

--- source/make_animations.py
+++ source/make_animations.py
@@ -3,6 +3,7 @@ from itertools import islice
 from tools.data_processing import process_trajectory ,combine_lw_sw_data, extract_hourly_ceres_data
 from tools.plotting import plot_radiance_animation, plot_radiance_geiger
 from tools.TLE_tools import sgp4_prop_TLE
+import concurrent.futures
 
 def main(dataset_path, TLE, jd_start, jd_end, dt, number_of_tsteps, output_folder):
     # Load the CERES dataset
@@ -30,8 +31,12 @@ def main(dataset_path, TLE, jd_start, jd_end, dt, number_of_tsteps, output_folde
                          combined_radiation_data, lat, lon, ceres_times, number_of_tsteps, 
                          lw=True, sw=True, lwsw=True, output_folder=output_folder)
 
+def process_satellite(dataset_path, TLE, jd_start, jd_end, dt, number_of_tsteps, output_folder):
+    # Assuming 'main' is your function that processes the satellite data
+    main(dataset_path, TLE, jd_start, jd_end, dt, number_of_tsteps=number_of_tsteps, output_folder=output_folder)
+
 if __name__ == "__main__":
-    # Configuration for OneWeb TLE and CERES dataset
+
     #oneweb TLE
     TLE_1 = "1 56719U 23068K   23330.91667824 -.00038246  00000-0 -10188+0 0  9993\n2 56719  87.8995  84.9665 0001531  99.5722 296.6576 13.15663544 27411"
     # a different oneweb satellite
@@ -40,13 +45,24 @@ if __name__ == "__main__":
     TLE_3 = "1 58214U 23170J   23345.43674150  .00003150  00000+0  17305-3 0  9997\n2 58214  42.9996 329.1219 0001662 255.3130 104.7534 15.15957346  7032"
     # a different starlink satellite
     TLE_4 = "1 56534U 23065AH  23345.43622091  .00010158  00000+0  76519-3 0  9998\n2 56534  43.0036 329.2697 0001189 268.3661  91.7045 15.02572222 32751"
-    jd_start = 2460069.5000000  # Force time to be within the CERES dataset
-    jd_end = jd_start + 1 # 1 day later
-    dt = 60  # Seconds
-    dataset_path = 'external\CERES_SYN1deg-1H_Terra-Aqua-MODIS_Ed4.1_Subset_20230501-20230630.nc'  # Hourly data
 
-    # # Execute the main function
-    main(dataset_path, TLE_1, jd_start, jd_end, dt, number_of_tsteps=600, output_folder='output/FOV_sliced_data/geiger_plots/oneweb_1/')
-    main(dataset_path, TLE_2, jd_start, jd_end, dt, number_of_tsteps=600, output_folder='output/FOV_sliced_data/geiger_plots/oneweb_2/')
-    main(dataset_path, TLE_3, jd_start, jd_end, dt, number_of_tsteps=600, output_folder='output/FOV_sliced_data/geiger_plots/starlink_1/')
-    main(dataset_path, TLE_4, jd_start, jd_end, dt, number_of_tsteps=600, output_folder='output/FOV_sliced_data/geiger_plots/starlink_2/')
+    # Configuration for satellites
+    satellites = [
+        {"TLE": TLE_1, "output_folder": "output/FOV_sliced_data/geiger_plots/oneweb_1/"},
+        {"TLE": TLE_2, "output_folder": "output/FOV_sliced_data/geiger_plots/oneweb_2/"},
+        {"TLE": TLE_3, "output_folder": "output/FOV_sliced_data/geiger_plots/starlink_1/"},
+        {"TLE": TLE_4, "output_folder": "output/FOV_sliced_data/geiger_plots/starlink_2/"}
+    ]
+
+    jd_start = 2460069.5000000
+    jd_end = jd_start + 1
+    dt = 60
+    no_steps = 600
+    dataset_path = 'external\CERES_SYN1deg-1H_Terra-Aqua-MODIS_Ed4.1_Subset_20230501-20230630.nc'
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = [executor.submit(process_satellite, dataset_path, sat["TLE"], jd_start, jd_end, dt, no_steps, sat["output_folder"]) for sat in satellites]
+
+        for future in concurrent.futures.as_completed(futures):
+            # You can add error handling or additional processing here
+            print(f"Task completed with result: {future.result()}")
