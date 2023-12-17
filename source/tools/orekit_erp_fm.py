ae669ac4f509cc9030e503890a01e9b2fee32a38
Charles C <66725307+CharlesPlusC@users.noreply.github.com>
2023-12-17 15:15:03 +0000

    orbit prop working- now to run the experiments...

--- source/tools/orekit_erp_fm.py
+++ source/tools/orekit_erp_fm.py
@@ -1,34 +1,17 @@
+
 import netCDF4 as nc
 import orekit
 from orekit.pyhelpers import setup_orekit_curdir
-from org.hipparchus.geometry.euclidean.threed import Vector3D, FieldVector3D
-from org.hipparchus.ode.nonstiff import DormandPrince853Integrator
+from org.hipparchus.geometry.euclidean.threed import Vector3D
 from org.orekit.forces import PythonForceModel
-from org.orekit.forces.gravity import HolmesFeatherstoneAttractionModel
-from org.orekit.forces.gravity.potential import GravityFieldFactory
 from org.orekit.frames import FramesFactory
-from org.orekit.orbits import KeplerianOrbit
-from org.orekit.orbits import OrbitType
-from org.orekit.orbits import PositionAngleType
-from org.orekit.propagation import SpacecraftState
-from org.orekit.propagation.numerical import NumericalPropagator
-from orekit import JArray_double
 from java.util import Collections
 from java.util.stream import Stream
 from org.orekit.time import AbsoluteDate
 from org.orekit.utils import Constants
 from org.orekit.utils import IERSConventions
-from org.orekit.propagation.numerical import NumericalPropagator
-from org.hipparchus.ode.nonstiff import DormandPrince853Integrator
-from org.orekit.propagation import SpacecraftState
 from org.orekit.bodies import OneAxisEllipsoid
 from org.orekit.utils import IERSConventions
-from org.orekit.forces.gravity.potential import GravityFieldFactory
-from org.orekit.forces.gravity import HolmesFeatherstoneAttractionModel
-from orekit import JArray_double
-from org.orekit.orbits import KeplerianOrbit, PositionAngleType
-from org.orekit.propagation.analytical import KeplerianPropagator
-from org.orekit.time import AbsoluteDate, TimeScalesFactory
 from org.orekit.utils import Constants
 from org.orekit.frames import FramesFactory
 
@@ -39,11 +22,8 @@ vm = orekit.initVM()
 setup_orekit_curdir()
 
 import numpy as np
-
 from tools.utilities import find_nearest_index, jd_to_utc, lla_to_ecef, julian_day_to_ceres_time
-from tools.data_processing import extract_hourly_ceres_data
-from tools.TLE_tools import twoLE_parse, tle_convert, sgp4_prop_TLE
-from tools.data_processing import extract_hourly_ceres_data ,combine_lw_sw_data, calculate_satellite_fov, is_within_fov_vectorized, sat_normal_surface_angle_vectorized
+from tools.data_processing import calculate_satellite_fov, is_within_fov_vectorized, sat_normal_surface_angle_vectorized
 
 def compute_erp_at_sc(ceres_time_index, radiation_data, sat_lat, sat_lon, sat_alt, horizon_dist):
     R = 6378.137  # Earth's radius in km
@@ -170,100 +150,3 @@ class CERES_ERP_ForceModel(PythonForceModel):
     def getEventDetectors(self):
         # No event detectors are used in this model
         return Stream.empty()
-
-if __name__ == "__main__":
-    dataset_path = 'external/data/CERES_SYN1deg-1H_Terra-Aqua-MODIS_Ed4.1_Subset_20230501-20230630.nc'  # Hourly data
-
-    data = nc.Dataset(dataset_path)
-
-    # Extract data from the CERES dataset
-    ceres_times, lat, lon, lw_radiation_data, sw_radiation_data = extract_hourly_ceres_data(data)
-
-    # Combine longwave and shortwave radiation data
-    combined_radiation_data = combine_lw_sw_data(lw_radiation_data, sw_radiation_data)
-
-    #oneweb TLE
-    # TLE = "1 56719U 23068K   23330.91667824 -.00038246  00000-0 -10188+0 0  9993\n2 56719  87.8995  84.9665 0001531  99.5722 296.6576 13.15663544 27411"
-    #starlink TLE
-    TLE = "1 58214U 23170J   23345.43674150  .00003150  00000+0  17305-3 0  9997\n2 58214  42.9996 329.1219 0001662 255.3130 104.7534 15.15957346  7032"
-    jd_start = 2460069.5000000  # Force time to be within the CERES dataset that I downloaded
-    jd_end = jd_start + 1/24 # 1 hr later
-    dt = 60  # Seconds
-    # sgp4_ephem = sgp4_prop_TLE(TLE=TLE, jd_start=jd_start, jd_end=jd_end, dt=dt)
-    # tle_time = TLE_time(TLE) ##The TLE I have is not actually in the daterange of the CERES dataset I downloaded so not using this now
-    utc_start = jd_to_utc(jd_start)
-
-    # Segment the time stamp into year, month, day, hour, minute, second components
-    YYYY = int(utc_start.strftime("%Y"))
-    MM = int(utc_start.strftime("%m"))
-    DD = int(utc_start.strftime("%d"))
-    H = int(utc_start.strftime("%H"))
-    M = int(utc_start.strftime("%M"))
-    S = float(utc_start.strftime("%S"))
-
-    #convert JD start epoch to UTC and pass to AbsoluteDate
-    utc = TimeScalesFactory.getUTC() #instantiate UTC time scale
-    #also add modified Julian date
-    TLE_epochDate = AbsoluteDate(YYYY, MM, DD, H, M, S, utc)
-    print("orekit AbsoluteDate:", TLE_epochDate)
-
-    #Convert the initial position and velocity to keplerian elements
-    tle_dict = twoLE_parse(TLE)
-    kep_elems = tle_convert(tle_dict)
-
-    a = float(kep_elems['a'])*1000
-    e = float(kep_elems['e'])
-    i = float(kep_elems['i'])
-    omega = float(kep_elems['arg_p'])
-    raan = float(kep_elems['RAAN'])
-    lv = (float(kep_elems['true_anomaly']))
-
-    ## Instantiate the inertial frame where the orbit is defined
-    inertialFrame = FramesFactory.getEME2000()
-
-    # ## Orbit construction as Keplerian
-    initialOrbit = KeplerianOrbit(a, e, i, omega, raan, lv,
-                                PositionAngleType.TRUE,
-                                inertialFrame, TLE_epochDate, Constants.WGS84_EARTH_MU)
-    print("initial orekit orbit:", initialOrbit)
-
-    # #Set parameters for numerical propagation
-    minStep = 0.001
-    maxstep = 1000.0
-    initStep = 60.0
-    positionTolerance = 1.0 
-    tolerances = NumericalPropagator.tolerances(positionTolerance, 
-                                                initialOrbit, 
-                                                initialOrbit.getType())
-    integrator = DormandPrince853Integrator(minStep, maxstep, 
-        JArray_double.cast_(tolerances[0]),  # Double array of doubles needs to be casted in Python
-        JArray_double.cast_(tolerances[1]))
-    integrator.setInitialStepSize(initStep)
-    satellite_mass = 500.0  # The models need a spacecraft mass, unit kg. 500kg is a complete guesstimate.
-
-    #Initial state
-    initialState = SpacecraftState(initialOrbit, satellite_mass) 
-    propagator_num = NumericalPropagator(integrator)
-    propagator_num.setOrbitType(OrbitType.CARTESIAN)
-    propagator_num.setInitialState(initialState)
-
-    print("initial SMA:", initialState.getA())
-
-    # Add 10x10 gravity field
-    gravityProvider = GravityFieldFactory.getNormalizedProvider(10, 10)
-    propagator_num.addForceModel(HolmesFeatherstoneAttractionModel(FramesFactory.getITRF(IERSConventions.IERS_2010, True), gravityProvider))
-    
-    #### ADDITION OF ERP CUSTOM FORCE MODEL ###
-
-    erp_force_model = CERES_ERP_ForceModel()
-    propagator_num.addForceModel(erp_force_model)
-
-    end_state = propagator_num.propagate(TLE_epochDate, TLE_epochDate.shiftedBy(3600.0 * 1))
-    end_state
-
-    print("Initial state:")
-    print(initialState)
-    print("Final state:")
-    print(end_state)
-    print("final altitude:", end_state.getA())
-    print("final orbit:", end_state.getOrbit())
