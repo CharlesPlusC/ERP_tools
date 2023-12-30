1a40592d81d48914e775b0757b54fd4d210e09d0
Charles C <66725307+CharlesPlusC@users.noreply.github.com>
2023-12-30 15:10:10 +0000

    seems like .py is a bit better

--- source/orbtdet_orekit_test.py
+++ source/orbtdet_orekit_test.py
@@ -50,7 +50,6 @@ from org.orekit.propagation.conversion import NumericalPropagatorBuilder
 from org.orekit.orbits import PositionAngleType
 
 
-
 def std_dev_from_lower_triangular(lower_triangular_data):
     cov_matrix = np.zeros((6, 6))
     row, col = np.tril_indices(6)
@@ -59,11 +58,12 @@ def std_dev_from_lower_triangular(lower_triangular_data):
     std_dev = np.sqrt(np.diag(cov_matrix))
     return std_dev
 
-
 def configure_force_models(propagatorBuilder,cr, cd, cross_section, enable_gravity=True, enable_third_body=True,
                         enable_solar_radiation=True, enable_relativity=True, enable_atmospheric_drag=True):
     # Earth gravity field with degree 64 and order 64
     if enable_gravity:
+        itrf = FramesFactory.getITRF(IERSConventions.IERS_2010, False)
+        ecef = itrf
         gravityProvider = GravityFieldFactory.getNormalizedProvider(64, 64)
         gravityAttractionModel = HolmesFeatherstoneAttractionModel(ecef, gravityProvider)
         propagatorBuilder.addForceModel(gravityAttractionModel)
@@ -107,7 +107,22 @@ def configure_force_models(propagatorBuilder,cr, cd, cross_section, enable_gravi
         propagatorBuilder.addForceModel(dragForce)
 
     return propagatorBuilder
-    
+
+# Function to create a symmetric correlation matrix from lower triangular covariance elements
+def create_symmetric_corr_matrix(lower_triangular_data):
+    # Create the symmetric covariance matrix
+    cov_matrix = np.zeros((6, 6))
+    row, col = np.tril_indices(6)
+    cov_matrix[row, col] = lower_triangular_data
+    cov_matrix = cov_matrix + cov_matrix.T - np.diag(cov_matrix.diagonal())
+
+    # Convert to correlation matrix
+    std_dev = np.sqrt(np.diag(cov_matrix))
+    corr_matrix = np.divide(cov_matrix, std_dev[:, None])
+    corr_matrix = np.divide(corr_matrix, std_dev[None, :])
+    np.fill_diagonal(corr_matrix, 1)  # Fill diagonal with 1s for self-correlation
+    return corr_matrix
+
 def spacex_ephem_to_df_w_cov(ephem_path: str) -> pd.DataFrame:
     """
     Convert SpaceX ephemeris data, including covariance terms, into a pandas DataFrame.
@@ -194,12 +209,6 @@ def spacex_ephem_to_df_w_cov(ephem_path: str) -> pd.DataFrame:
 def main():
     ephem_path = '/Users/charlesc/Documents/GitHub/ERP_tools/external/ephems/starlink/MEME_57632_STARLINK-30309_3530645_Operational_1387262760_UNCLASSIFIED.txt'
     spacex_ephem_dfwcov = spacex_ephem_to_df_w_cov(ephem_path)
-    print(spacex_ephem_dfwcov.head())
-
-    plt.scatter(spacex_ephem_dfwcov['hours'][0:600],spacex_ephem_dfwcov['sigma_pos'][0:600], s=1)
-    plt.xlabel('Hours from epoch')
-    plt.ylabel('Mean Position uncertainty (km)')
-    plt.grid()
 
     sat_list = {    
     'STARLINK-30309': {
@@ -215,21 +224,15 @@ def main():
 
     sc_name = 'STARLINK-30309'  # Change the name to select a different satellite in the dict
 
-    range_weight = 1.0 # Will be normalized later (i.e divided by the number of observations)
-    range_sigma = 1.0 # Estimated covariance of the range measurements, in meters
-
     odDate = datetime(2023, 12, 19, 6, 45, 42, 00000)
     collectionDuration = 1 * 1/24 * 1/60 * 5 # 5 minutes
     startCollectionDate = odDate + timedelta(days=-collectionDuration)
 
     #Get TLE for first guess
-
     # Space-Track
     identity_st = input('Enter SpaceTrack username')
     password_st = getpass.getpass(prompt='Enter SpaceTrack password for account {}'.format(identity_st))
-
     st = SpaceTrackClient(identity=identity_st, password=password_st)
-
     rawTle = st.tle(norad_cat_id=sat_list[sc_name]['norad_id'], epoch='<{}'.format(odDate), orderby='epoch desc', limit=1, format='tle')
     tleLine1 = rawTle.split('\n')[0]
     tleLine2 = rawTle.split('\n')[1]
@@ -246,12 +249,9 @@ def main():
     estimator_convergence_thres = 1e-2
     estimator_max_iterations = 25
     estimator_max_evaluations = 35
-    tod = FramesFactory.getTOD(IERSConventions.IERS_2010, False) # Taking tidal effects into account when interpolating EOP parameters
     gcrf = FramesFactory.getGCRF()
     # Selecting frames to use for OD
     eci = gcrf
-    utc = TimeScalesFactory.getUTC()
-    # mjd_utc_epoch = AbsoluteDate(1858, 11, 17, 0, 0, 0.0, utc)
     orekitTle = TLE(tleLine1, tleLine2)
     itrf = FramesFactory.getITRF(IERSConventions.IERS_2010, False)
     ecef = itrf
@@ -281,13 +281,12 @@ def main():
         propagatorBuilder = NumericalPropagatorBuilder(tleOrbit_ECI, integratorBuilder, PositionAngleType.MEAN, estimator_position_scale)
         propagatorBuilder.setMass(sat_list[sc_name]['mass'])
         propagatorBuilder.setAttitudeProvider(nadirPointing)
-        configured_propagatorBuilder = configure_force_models(propagatorBuilder,sat_list[sc_name]['cr'], sat_list[sc_name]['cd'],sat_list[sc_name]['cross_section'], **config)
+        configured_propagatorBuilder = configure_force_models(propagatorBuilder,sat_list[sc_name]['cr'], sat_list[sc_name]['cd'],
+                                                              sat_list[sc_name]['cross_section'], **config)
         propagatorBuilders.append(configured_propagatorBuilder)
 
-    # Initialize a dictionary to store results for each propagatorBuilder
     results = {}
-
-    # Outer loop over each propagatorBuilder
+    # Outer loop over each propagatorBuilder (/force model configuration)
     for idx, propagatorBuilder in enumerate(propagatorBuilders):
         print(f"Running for configuration {idx}")
         print(f"propagatorBuilder: {propagatorBuilder}")
@@ -300,13 +299,7 @@ def main():
         estimator.setMaxIterations(estimator_max_iterations)
         estimator.setMaxEvaluations(estimator_max_evaluations)
 
-        ranges_used = []
-        mean_pos_std_list = []
-        cross_track_list = []
-        along_track_list = []
-        out_of_plane_list = []
-
-        # Inner loop over points_to_use in OD
+        # Try different number fo points to use to use in OD process
         for points_to_use in range(1, 150, 45):
             print(f"Running for {points_to_use} points with configuration {idx}")
 
@@ -348,16 +341,7 @@ def main():
             covarianceMat_lvlh = np.matrix([covMat_lvlh_java.getRow(iRow)
                                             for iRow in range(0, covMat_lvlh_java.getRowDimension())])
 
-            pos_std_crossTrack = np.sqrt(covarianceMat_lvlh[0, 0])
-            pos_std_alongTrack = np.sqrt(covarianceMat_lvlh[1, 1])
-            pos_std_outOfPlane = np.sqrt(covarianceMat_lvlh[2, 2])
-
-            mean_pos_std = np.mean([pos_std_crossTrack, pos_std_alongTrack, pos_std_outOfPlane])
-            ranges_used.append(points_to_use)
-            mean_pos_std_list.append(mean_pos_std)
-            cross_track_list.append(pos_std_crossTrack)
-            along_track_list.append(pos_std_alongTrack)
-            out_of_plane_list.append(pos_std_outOfPlane)
+            # mean_pos_std = np.mean([pos_std_crossTrack, pos_std_alongTrack, pos_std_outOfPlane])
 
             import seaborn as sns
             import matplotlib.pyplot as plt
@@ -370,15 +354,13 @@ def main():
             plt.savefig(f"covMat_lvlh_{points_to_use}_{idx}.png")
             plt.show()
 
-
-        # Store results in the dictionary
-        results[idx] = {
-            'ranges_used': ranges_used,
-            'cross_track': cross_track_list,
-            'along_track': along_track_list,
-            'out_of_plane': out_of_plane_list,
-            'mean_pos_std': mean_pos_std_list
-        }
+            # Create and Plot the Correlation Matrix
+            lower_triangular_data = covarianceMat_lvlh[np.tril_indices_from(covarianceMat_lvlh)]
+            corr_matrix = create_symmetric_corr_matrix(lower_triangular_data)
+            sns.heatmap(corr_matrix, annot=True, fmt=".2f", xticklabels=labels, yticklabels=labels, cmap="coolwarm", center=0)
+            plt.title(f"Correlation Matrix - points_to_use = {points_to_use}, configuration = {idx}")
+            plt.savefig(f"corrMat_lvlh_{points_to_use}_{idx}.png")
+            plt.show()
 
     for idx, data in results.items():
         plt.plot(data['ranges_used'], data['out_of_plane'], label=f'Config {idx}')
@@ -390,3 +372,6 @@ def main():
     plt.legend()
     plt.grid(True)
     plt.show()
+
+if __name__ == '__main__':
+    main()
