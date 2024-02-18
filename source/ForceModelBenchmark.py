0908fcec91953643e04de1d5351326a2f23a0be9
Charles C <66725307+CharlesPlusC@users.noreply.github.com>
2024-02-18 16:28:29 +0000

    save some sunday runs...

--- source/ForceModelBenchmark.py
+++ source/ForceModelBenchmark.py
@@ -70,8 +70,8 @@ def main():
         cross_section = sat_info['cross_section']
         mass = sat_info['mass']
         ephemeris_df = ephemeris_df.iloc[::2, :]
-        #slice the ephemeris to start 6 arcs past the beginning
-        ephemeris_df = ephemeris_df.iloc[6*arc_length:]
+        #slice the ephemeris to start 7 arcs past the beginning
+        ephemeris_df = ephemeris_df.iloc[7*arc_length:]
         time_step = (ephemeris_df['UTC'].iloc[1] - ephemeris_df['UTC'].iloc[0]).total_seconds() / 60.0  # in minutes
         time_step_seconds = time_step * 60.0
         arc_step = int(arc_length / time_step)
