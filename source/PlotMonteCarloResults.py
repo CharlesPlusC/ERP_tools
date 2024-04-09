37f30314cb42c2d5f47a4f096387ee3ac26985cc
Charles C <66725307+CharlesPlusC@users.noreply.github.com>
2024-04-09 13:06:11 +0100

    edits to monte carlo plots

--- source/PlotMonteCarloResults.py
+++ source/PlotMonteCarloResults.py
@@ -21,7 +21,7 @@ def plot_tca_vs_dca(dataframes, filenames, save_path, sat_name):
 
         # Draw a combo histogram and scatterplot with density contours
         sns.scatterplot(x=tca_seconds, y=dca, s=5, color=".15", ax=ax)
-        sns.histplot(x=tca_seconds, y=dca, bins=50, pthresh=.1, cmap="mako", ax=ax)
+        sns.histplot(x=tca_seconds, y=dca, bins=50, pthresh=.1, cmap="rocket", ax=ax)
         sns.kdeplot(x=tca_seconds, y=dca, levels=5, color="w", linewidths=1, ax=ax)
 
         ax.set_title(f'{sat_name} - TCA vs. DCA ({filename})')
@@ -47,7 +47,7 @@ def plot_tca_distributions_facetgrid(dataframes, filenames, save_path, sat_name)
     max_tca = max(abs(combined_df['TCA'].min()), combined_df['TCA'].max())
 
     # Create the FacetGrid object
-    pal = sns.cubehelix_palette(len(dataframes), rot=-.25, light=.7)
+    pal = sns.cubehelix_palette(len(dataframes), light=.7)
     g = sns.FacetGrid(combined_df, row="Label", hue="Label", aspect=15, height=.5, palette=pal)
 
     # Draw the densities and add the red line at TCA=0
@@ -69,32 +69,86 @@ def plot_tca_distributions_facetgrid(dataframes, filenames, save_path, sat_name)
     def label(x, color, label):
         ax = plt.gca()
         ax.text(0, .2, label, fontweight="bold", color=color,
-                ha="left", va="center", transform=ax.transAxes, fontsize=9)
+                ha="left", va="center", transform=ax.transAxes, fontsize=11)
 
     g.map(label, "TCA")
 
-    # Adjust subplot parameters and remove axes details that don't play well with overlap
-    g.figure.subplots_adjust(hspace=-.25)
+    g.set_axis_labels("ΔTCA (seconds)")
     g.set_titles("")
     g.set(yticks=[], ylabel="")
     g.despine(bottom=True, left=True)
 
     # Add the overarching title
-    plt.suptitle(f'{sat_name} - TCA Distributions', y=1.02, fontsize=16)
+    plt.suptitle(f'{sat_name} - TCA Distributions', fontsize=14)
+    plt.tight_layout()
 
     plt.savefig(os.path.join(save_path, f'{sat_name}_TCA_Distributions_FacetGrid.png'))
     plt.close()
 
+def plot_dca_distributions_facetgrid(dataframes, filenames, save_path, sat_name):
+    combined_df = pd.DataFrame()
+    for df, filename in zip(dataframes, filenames):
+        unique_id = filename.split('_')[3]  # Adjust based on your filename structure
+        label = f"fm{unique_id}"
+        dca_values = df['DCA']
+        temp_df = pd.DataFrame({'DCA': dca_values, 'Label': label})
+        combined_df = pd.concat([combined_df, temp_df])
+
+    # Find the maximum DCA value to set symmetrical x-axis limits
+    max_dca = max(abs(combined_df['DCA'].min()), combined_df['DCA'].max())
+
+    # Create the FacetGrid object
+    pal = sns.cubehelix_palette(len(dataframes), light=.7)
+    g = sns.FacetGrid(combined_df, row="Label", hue="Label", aspect=15, height=.5, palette=pal)
+
+    # Draw the densities
+    g.map(sns.kdeplot, "DCA", bw_adjust=.5, clip_on=False, fill=True, alpha=1, linewidth=1.5)
+
+    # Add vertical lines for mean and quartiles and a reference line at DCA=0
+    def add_lines(x, **kwargs):
+        mean = np.mean(x)
+        q1 = np.percentile(x, 25)
+        q3 = np.percentile(x, 75)
+        plt.axvline(mean, color='green', linestyle='--')
+        plt.axvline(q1, color='blue', linestyle=':')
+        plt.axvline(q3, color='blue', linestyle=':')
+        plt.axvline(0, color='red', linestyle='--')
+
+    g.map(add_lines, "DCA")
+
+    # Function to label the plot in axes coordinates
+    def label(x, color, label):
+        ax = plt.gca()
+        ax.text(0, .2, label, fontweight="bold", color=color,
+                ha="left", va="center", transform=ax.transAxes, fontsize=11)
+
+    g.map(label, "DCA")
+
+    # Set symmetrical x-axis limits based on the maximum DCA value
+    g.set(xlim=(-max_dca, max_dca))
+
+    g.set_axis_labels("DCA (meters)")
+    g.set_titles("")
+    g.set(yticks=[], ylabel="")
+    g.despine(bottom=True, left=True)
+
+    plt.suptitle(f'{sat_name} - DCA Distributions', fontsize=14)
+    plt.tight_layout()
+
+    plt.savefig(os.path.join(save_path, f'{sat_name}_DCA_Distributions_FacetGrid.png'))
+    plt.close()
+
+
 # Function to plot the probability of collision estimate and save the plot
 def plot_collision_probability_estimate(probabilities, filenames, save_path, sat_name):
     fig, ax = plt.subplots()
-    ax.bar(filenames, probabilities, color='skyblue')
-    ax.set_xlabel('File')
+    ax.bar(filenames, probabilities, color='orange')
+    ax.set_xlabel('Force Model')
     ax.set_ylabel('Probability of Collision (%)')
     ax.set_title(f'{sat_name} - Probability of Collision Estimate')
     ax.set_yscale('log')
     ax.set_ylim(1e-4, 100)
-    ax.grid(True)
+    ax.grid(color='black', linestyle='-', linewidth=0.3)
 
     for i, v in enumerate(probabilities):
         ax.text(i, max(v, 1e-4) * 1.1, f"{v:.4f}%", ha='center', va='bottom')
@@ -109,6 +163,7 @@ def plot_cumulative_distribution_dca(dataframes, filenames, save_path, sat_name)
     for df, filename in zip(dataframes, filenames):
         dca_sorted = np.sort(df['DCA'].values)
         ax.plot(dca_sorted, np.linspace(0, 100, len(dca_sorted)), label=filename)
+        ax.grid(color='black', linestyle='-', linewidth=0.5)
 
     ax.set_title(f'{sat_name} - Cumulative Distribution of DCA')
     ax.set_xlabel('DCA (m)')
@@ -118,48 +173,6 @@ def plot_cumulative_distribution_dca(dataframes, filenames, save_path, sat_name)
 
     plt.tight_layout()
     plt.savefig(os.path.join(save_path, f'{sat_name}_Cumulative_Distribution_DCA.png'))
-    plt.close()
-
-# Function to plot symmetrical histograms and line plots of TCA sample numbers for each file
-def plot_tca_histograms_and_sample_lines(dataframes, filenames, save_path, sat_name):
-    combined_fig, combined_ax = plt.subplots()  # For the combined plot of all TCA lines
-
-    for df, filename in zip(dataframes, filenames):
-        fig, ax = plt.subplots()
-        tca_seconds = (pd.to_datetime(df['TCA']) - pd.to_datetime(df['TCA'].iloc[0])).dt.total_seconds()
-        median_tca = np.median(tca_seconds)
-        
-        # Creating symmetric bins and plotting histogram
-        max_range = max(median_tca - min(tca_seconds), max(tca_seconds) - median_tca)
-        bins = np.arange(median_tca - max_range, median_tca + max_range + 0.001, 0.001)
-        counts, edges = np.histogram(tca_seconds, bins=bins)
-
-        ax.hist(tca_seconds, bins=bins, color='orange', edgecolor='black', alpha=0.5)
-        bin_centers = 0.5 * (edges[1:] + edges[:-1])
-        ax.plot(bin_centers, counts, '-k')
-
-        ax.set_xlim(median_tca - max_range, median_tca + max_range)
-
-        ax.set_title(f'{sat_name} - TCA Histogram ({filename})')
-        ax.set_xlabel('Time of Closest Approach (seconds)')
-        ax.set_ylabel('Number of Samples')
-        ax.grid(True)
-
-        plt.tight_layout()
-        plt.savefig(os.path.join(save_path, f'{sat_name}_{filename}_TCA_Histogram_Sample_Numbers.png'))
-        plt.close()
-
-        # Adding the line plot to the combined plot
-        combined_ax.plot(bin_centers, counts, label=filename)
-
-    combined_ax.set_title(f'{sat_name} - Combined TCA Sample Number Lines')
-    combined_ax.set_xlabel('Time of Closest Approach (seconds)')
-    combined_ax.set_ylabel('Number of Samples')
-    combined_ax.grid(True)
-    combined_ax.legend()
-
-    plt.tight_layout()
-    plt.savefig(os.path.join(save_path, f'{sat_name}_Combined_TCA_Sample_Number_Lines.png'))
     plt.close()
 
 # Function to plot TCA vs. DCA in a 3x3 subplot matrix
@@ -187,9 +200,9 @@ def plot_tca_vs_dca_matrix(dataframes, filenames, save_path, sat_name):
         axs[i].grid(True)
         axs[i].legend()
         #make the y lim from 0-12
-        axs[i].set_ylim(0, 12)
+        # axs[i].set_ylim(0, 12)
         #make the x lim from -0.008 to 0.008
-        axs[i].set_xlim(-0.008, 0.008)
+        # axs[i].set_xlim(-0.008, 0.008)
         #add a black grid
         axs[i].grid(color='black', linestyle='-', linewidth=0.5)
 
@@ -226,7 +239,18 @@ def plot_tca_vs_dca_jointplot(dataframes, filenames, save_path, sat_name):
         space=0,
     )
 
-    plt.suptitle(f'{sat_name} - TCA vs. DCA KDE Comparison', y=1.02)
+    #change x label to Time of Closest Approach (seconds)
+    g.set_axis_labels("ΔTCA (seconds)", "ΔDCA (meters)")
+
+    #add grid with black color
+    g.ax_joint.grid(color='black', linestyle='-', linewidth=0.3)
+
+    #set figure title
+    g.figure.suptitle(f'{sat_name} - TCA vs. DCA KDE')
+
+    #tight layout
+    plt.tight_layout()
+
     plt.savefig(os.path.join(save_path, f'{sat_name}_TCA_vs_DCA_KDE.png'))
     plt.close()
 
@@ -252,10 +276,10 @@ for sat_name in sat_names_to_test:
 
     plot_collision_probability_estimate(collision_probabilities, filenames, save_path, sat_name)
     plot_cumulative_distribution_dca(dataframes, filenames, save_path, sat_name)
-    plot_tca_histograms_and_sample_lines(dataframes, filenames, save_path, sat_name)
     plot_tca_vs_dca(dataframes, filenames, save_path, sat_name)
     plot_tca_vs_dca_matrix(dataframes, files, save_path, sat_name)
     plot_tca_distributions_facetgrid(dataframes, files, save_path, sat_name)
+    plot_dca_distributions_facetgrid(dataframes, files, save_path, sat_name)
     plot_tca_vs_dca_jointplot(dataframes, files, save_path, sat_name)
 
 
