fix and clean up interpolation and numerical differentiation functions

source/GFOAccelerometryBenchmark.py
@@ -17,17 +17,16 @@ from tools.GFODataReadTools import get_gfo_inertial_accelerations
 
 def compute_acc_from_vel(sat_name = "GRACE-FO-A", 
                          start_date = datetime.datetime(2023, 5, 5, 0, 0, 0), 
-                         end_date = datetime.datetime(2023, 5, 5, 6, 0, 0),
+                         end_date = datetime.datetime(2023, 5, 5, 2, 0, 0),
                          window_length=21, polyorder=7,
-                         ephemeris_interp_freq='0.01S', downsample_freq='15S'):
+                         ephemeris_interp_freq='0.01S', downsample_freq='1S'):
     
     sat_info = get_satellite_info(sat_name)
     ephemeris_df = sp3_ephem_to_df(sat_name)
     ephemeris_df = ephemeris_df[(ephemeris_df['UTC'] >= start_date) & (ephemeris_df['UTC'] <= end_date)]
 
     inverted_accelerations_df = pd.DataFrame(columns=['utc_time', 'inertial_x_acc', 'inertial_y_acc', 'inertial_z_acc'])
-    force_model_config = {'120x120gravity': True, '3BP': True,'solid_tides': True, 'ocean_tides': True, 'relativity': True},
-
+   
     settings = {
         'cr': sat_info['cr'], 'cd': sat_info['cd'], 'cross_section': sat_info['cross_section'], 'mass': sat_info['mass'],
         'filter_window_length': window_length, 'filter_polyorder': polyorder,
@@ -35,22 +34,37 @@ def compute_acc_from_vel(sat_name = "GRACE-FO-A",
     }
     
     interp_ephemeris_df = interpolate_positions(ephemeris_df, settings['ephemeris_interp_freq'])
+    print(f"columns in interp_ephemeris_df: {interp_ephemeris_df.columns}")
     interp_ephemeris_df = calculate_acceleration(interp_ephemeris_df, 
                                                     settings['ephemeris_interp_freq'],
                                                     settings['filter_window_length'],
                                                     settings['filter_polyorder'])
-
+    print(f"columns in interp_ephemeris_df after calculating acceleration: {interp_ephemeris_df.columns}")
     interp_ephemeris_df['UTC'] = pd.to_datetime(interp_ephemeris_df['UTC'])
     interp_ephemeris_df.set_index('UTC', inplace=True)
     interp_ephemeris_df = interp_ephemeris_df.asfreq(settings['downsample_freq'])
 
     for i in tqdm(range(1, len(interp_ephemeris_df)), desc='Computing Accelerations'):
         epoch = interp_ephemeris_df.index[i]
-        vel = np.array([interp_ephemeris_df['xv'][i], interp_ephemeris_df['yv'][i], interp_ephemeris_df['zv'][i]])
-        state_vector = np.array([interp_ephemeris_df['x'][i], interp_ephemeris_df['y'][i], interp_ephemeris_df['z'][i], vel[0], vel[1], vel[2]])
+        vel = np.array([
+            interp_ephemeris_df['xv'].iloc[i],
+            interp_ephemeris_df['yv'].iloc[i],
+            interp_ephemeris_df['zv'].iloc[i]
+        ])
+        state_vector = np.array([
+            interp_ephemeris_df['x'].iloc[i],
+            interp_ephemeris_df['y'].iloc[i],
+            interp_ephemeris_df['z'].iloc[i],
+            vel[0], vel[1], vel[2]
+        ])
+        force_model_config = {'120x120gravity': True, '3BP': True}
         conservative_accelerations = state2acceleration(state_vector, epoch, settings['cr'], settings['cd'], settings['cross_section'], settings['mass'], **force_model_config)
         computed_accelerations_sum = np.sum(list(conservative_accelerations.values()), axis=0)
-        observed_acc = np.array([interp_ephemeris_df['accx'][i], interp_ephemeris_df['accy'][i], interp_ephemeris_df['accz'][i]])
+        observed_acc = np.array([
+            interp_ephemeris_df['accx'].iloc[i],
+            interp_ephemeris_df['accy'].iloc[i],
+            interp_ephemeris_df['accz'].iloc[i]
+        ])
         diff = computed_accelerations_sum - observed_acc
         diff_x, diff_y, diff_z = diff[0], diff[1], diff[2]
         new_row = {
@@ -58,18 +72,18 @@ def compute_acc_from_vel(sat_name = "GRACE-FO-A",
             'inverted_x_acc': diff_x,
             'inverted_y_acc': diff_y,
             'inverted_z_acc': diff_z,
-            'observed_x_acc': interp_ephemeris_df['accx'][i],
-            'observed_y_acc': interp_ephemeris_df['accy'][i],
-            'observed_z_acc': interp_ephemeris_df['accz'][i],
+            'observed_x_acc': interp_ephemeris_df['accx'].iloc[i],
+            'observed_y_acc': interp_ephemeris_df['accy'].iloc[i],
+            'observed_z_acc': interp_ephemeris_df['accz'].iloc[i],
             'computed_x_acc': computed_accelerations_sum[0],
             'computed_y_acc': computed_accelerations_sum[1],
             'computed_z_acc': computed_accelerations_sum[2],
-            'x': interp_ephemeris_df['x'][i],
-            'y': interp_ephemeris_df['y'][i],
-            'z': interp_ephemeris_df['z'][i],
-            'xv': interp_ephemeris_df['xv'][i],
-            'yv': interp_ephemeris_df['yv'][i],
-            'zv': interp_ephemeris_df['zv'][i],}
+            'x': interp_ephemeris_df['x'].iloc[i],
+            'y': interp_ephemeris_df['y'].iloc[i],
+            'z': interp_ephemeris_df['z'].iloc[i],
+            'xv': interp_ephemeris_df['xv'].iloc[i],
+            'yv': interp_ephemeris_df['yv'].iloc[i],
+            'zv': interp_ephemeris_df['zv'].iloc[i],}
         inverted_accelerations_df = pd.concat([inverted_accelerations_df, pd.DataFrame([new_row])])
 
     datenow = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
@@ -81,46 +95,60 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.signal import welch, coherence
 
-def compute_and_plot_psd_coherence(df, components, window_lengths, fs=1/15, nperseg=256):
+def compute_and_plot_psd_coherence(df, components, window_lengths, fs=1, nperseg=256):
     for window_length in window_lengths:
         fig, axes = plt.subplots(nrows=len(components), ncols=2, figsize=(15, 8), sharex=True)
-        for i, component in enumerate(components):
+        min_psd, max_psd, max_coh = float('inf'), float('-inf'), float('-inf')
+
+        # First pass to determine the global min and max for PSD, max for Coherence
+        for component in components:
             signal_inverted = df[f'inverted_{component}_acc'].rolling(window=window_length, center=True).mean().dropna()
-            signal_inertial = df[f'inertial_{component}_acc'].rolling(window=window_length, center=True).mean().dropna()
+            signal_inertial = df[f'inertial_{component}_acc']#.rolling(window=window_length, center=True).mean().dropna()
 
             # PSD and Coherence calculation
             f_inv, Pxx_inv = welch(signal_inverted, fs=fs, nperseg=nperseg)
             f_iner, Pxx_iner = welch(signal_inertial, fs=fs, nperseg=nperseg)
+            _, Cxy = coherence(signal_inverted, signal_inertial, fs=fs, nperseg=nperseg)
+
+            min_psd = min(min_psd, np.min(Pxx_inv), np.min(Pxx_iner))
+            max_psd = max(max_psd, np.max(Pxx_inv), np.max(Pxx_iner))
+            max_coh = max(max_coh, np.max(Cxy))
+
+        # Second pass to plot using consistent axes
+        for i, component in enumerate(components):
+            signal_inverted = df[f'inverted_{component}_acc'].rolling(window=window_length, center=True).mean().dropna()
+            signal_inertial = df[f'inertial_{component}_acc']#.rolling(window=window_length, center=True).mean().dropna()
+
+            # PSD and Coherence calculation again
+            f_inv, Pxx_inv = welch(signal_inverted, fs=fs, nperseg=nperseg)
+            f_iner, Pxx_iner = welch(signal_inertial, fs=fs, nperseg=nperseg)
             f_coh, Cxy = coherence(signal_inverted, signal_inertial, fs=fs, nperseg=nperseg)
 
-            # Convert frequency from Hz to period in minutes (90 minutes as reference cycle)
-            period_inv = 1 / f_inv
-            period_iner = 1 / f_iner
-            period_coh = 1 / f_coh
             # Plot PSD
-            axes[i, 0].semilogy(period_inv, Pxx_inv, label=f'Inverted {component}')
-            axes[i, 0].semilogy(period_iner, Pxx_iner, label=f'Inertial {component}')
+            axes[i, 0].plot(f_inv, Pxx_inv, label=f'Inverted {component}')
+            axes[i, 0].plot(f_iner, Pxx_iner, label=f'Inertial {component}')
             axes[i, 0].set_title(f'PSD for {component} Component')
-            axes[i, 0].set_xlabel('Period (Minutes)')
-            axes[i, 0].set_ylabel('PSD')
+            axes[i, 0].set_xlabel('Frequency (Hz)')
+            axes[i, 0].set_ylabel('PSD (m/s^-2 Hz^-0.5)')
             axes[i, 0].legend()
-            #plot from 0-180 minutes
-            axes[i, 0].set_xlim(0, 180)
-
+            axes[i, 0].set_yscale('log')
+            axes[i, 0].set_xscale('log')
+            axes[i, 0].set_ylim([min_psd, max_psd])
+            axes[i, 0].grid(which='both', linestyle='--')
 
             # Plot Coherence
-            axes[i, 1].plot(period_coh, Cxy, label=f'Coherence {component}')
+            axes[i, 1].plot(f_coh, Cxy, label=f'Coherence {component}')
             axes[i, 1].set_title(f'Coherence for {component} Component')
-            axes[i, 1].set_xlabel('Period (Minutes)')
+            axes[i, 1].set_xlabel('Frequency (Hz)')
             axes[i, 1].set_ylabel('Coherence')
+            axes[i, 1].set_xscale('log')
             axes[i, 1].legend()
-            #plot from 0-180 minutes
-            axes[i, 1].set_xlim(0, 180)
+            axes[i, 1].set_ylim([0, max_coh])
+            axes[i, 1].grid(which='both', linestyle='--')
         
         plt.tight_layout()
-        plt.savefig(f"output/PSD_Coherence_Comparison_{window_length}.png")
+        plt.savefig(f"output/DensityInversion/PODBasedAccelerometry/Plots/AccelerometerBenchmarking/PSD/PSD_Coherence_Comparison_{window_length}.png")
         plt.close()
-
 def avg_win_length_rms(merged_df):
     #plot the RMS difference between the inverted and measured accelerations as a function of averaging window size
     #used to select the averaging window size that gives the best agreement between the inverted and measured accelerations
@@ -148,11 +176,11 @@ def avg_win_length_rms(merged_df):
             plt.ylabel(f'{component} Acceleration (m/s^2)')
             plt.legend()
             #set ylim from 1e-8 to 1e-6
-            plt.ylim(1e-8, 1e-6)
+            plt.ylim(1e-8, 1e-5)
             #log scale
             plt.yscale('log')
             plt.tight_layout()
-            plt.savefig(f"output/DensityInversion/PODBasedAccelerometry/Plots/AccelerometerBenchmarking/{component}_diff_comparison{window}.png")
+            plt.savefig(f"output/DensityInversion/PODBasedAccelerometry/Plots/AccelerometerBenchmarking/HCLAccDiffs/{component}_diff_comparison{window}.png")
             plt.close()
 
         #plot the RMS difference as a function of window size
@@ -160,7 +188,7 @@ def avg_win_length_rms(merged_df):
         plt.xlabel('Window Size')
         plt.ylabel('RMS Difference')
         plt.tight_layout()
-        plt.savefig(f"output/DensityInversion/PODBasedAccelerometry/Plots/AccelerometerBenchmarking/{component}_diff_RMS_Difference.png")
+        plt.savefig(f"output/DensityInversion/PODBasedAccelerometry/Plots/AccelerometerBenchmarking/HCLAccDiffs/{component}_diff_RMS_Difference.png")
         plt.close()
 
 if __name__ == '__main__':
@@ -199,7 +227,7 @@ if __name__ == '__main__':
         merged_df.loc[i, 'inertial_c_acc'] = c_diff_meas
         merged_df.loc[i, 'inertial_l_acc'] = l_diff_meas
     
-    # avg_win_length_rms(merged_df)
+    avg_win_length_rms(merged_df)
 
     components = ['h', 'c', 'l']
     window_lengths = range(5, 256, 5)

source/tools/utilities.py
@@ -760,16 +760,19 @@ def interpolate_positions(df, fine_freq):
         df_interpolated[pos_col] = CubicSpline(times, df[pos_col])(new_times)
         df_interpolated[vel_col] = CubicSpline(times, df[vel_col])(new_times)
 
+    df_interpolated.reset_index(inplace=True)
+    df_interpolated.rename(columns={'index': 'UTC'}, inplace=True)
+
     return df_interpolated
 
 def calculate_acceleration(df_interpolated, fine_freq, filter_window_length, filter_polyorder):
     fine_freq_seconds = pd.to_timedelta(fine_freq).total_seconds()
-    for vel_col in ['xv', 'yv', 'zv']:
+    for vel_col, acc_col in zip(['xv', 'yv', 'zv'], ['accx', 'accy', 'accz']):
         velocities = df_interpolated[vel_col].to_numpy()
         if filter_window_length > len(velocities):
-            filter_window_length = len(velocities) | 1
+            filter_window_length = len(velocities) | 1  # Ensure the window length is odd
         df_interpolated[vel_col] = savgol_filter(velocities, filter_window_length, filter_polyorder)
-        df_interpolated['acc' + vel_col[1]] = np.gradient(df_interpolated[vel_col], fine_freq_seconds)
+        df_interpolated[acc_col] = np.gradient(df_interpolated[vel_col], fine_freq_seconds)
 
     return df_interpolated
 